0c80b0679da0e9041c17ede6e1c179afa2b35d92
grac2801 <oscar.gracias@icloud.com>
2026-08-04 09:40:28 -0700

    U2_L2_escapeKeys

---
+++ unit002_usingObjects/U2_L2_escapeSequence_StringConcatenation.java
@@ -0,0 +1,31 @@
+package unit002_usingObjects;
+
+public class U2_L2_escapeSequence_StringConcatenation
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * 1) + is used as a concatenator or operation, depending
+		 * on data type.
+		 * 2) Escape keys [started with \]
+		 */
+		
+		System.out.println("\"Everybody should learn to program a computer,\n because it teaches you how to think.\" \n\t\t-Steve Jobs, former CEO and creator of Apple");
+		//print Users\123456\myFile.java
+		System.out.println("Users\\123456\\myFile.java");
+		
+		/*
+		 *\"   double quote 
+		 *\n   new line
+		 *\t   horizontal tab
+		 *\\   backslash
+		 *\'   single quote
+		 *
+		 *
+		 *
+		 */
+		
+	}
+
+}
